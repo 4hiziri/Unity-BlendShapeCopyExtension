a050238e0d3c6f7e4b135d8e259dc2f94f9023e8
4hiziri <meirvg@gmail.com>
2021-08-03 01:21:05 +0900

    Add code and readme

---
+++ Editor/ShapeKeyCopyTool.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace BlendShapeCopyTool {
+    public class BlendShapeCopyMenu : MonoBehaviour {
+        [SerializeField]
+        static List<(string, float)> _copiedBlendShapes = new List<(string, float)>();
+
+        [MenuItem("CONTEXT/SkinnedMeshRenderer/Copy BlendShapes", false, 120)]
+        static private void CopyBlendShape(MenuCommand cmd) {
+            SkinnedMeshRenderer smrCmp = cmd.context as SkinnedMeshRenderer;
+            List<(string, float)> blendshapes = ExtractBlendShapes(smrCmp);
+
+            _copiedBlendShapes = blendshapes;
+
+            Debug.Log("Copy: success");
+        }
+
+        [MenuItem("CONTEXT/SkinnedMeshRenderer/Paste BlendShapes Strictly", false, 120)]
+        static private void PasteBlendShapeStrictly(MenuCommand cmd) {
+            if (_copiedBlendShapes == null) {
+                Debug.LogAssertion("PasteBlendShapeStrictly: _copiedBlendShapes is null");
+                return;
+            }
+
+            SkinnedMeshRenderer smrCmp = cmd.context as SkinnedMeshRenderer;
+            List<(string, float)> blendShapes = ExtractBlendShapes(smrCmp);
+
+            if (IsSameBlendShapes(_copiedBlendShapes, blendShapes)) {
+                Debug.Log("PasteBlendShapeStrictly: start to paste");
+                Undo.RecordObject(smrCmp as Object, "Paste BlendShapes");
+                MovBlendShapes(smrCmp);
+            } else {
+                Debug.Log("PasteBlendShapeStrictly: doesn't equal blendShapes");
+            }
+        }
+
+        [MenuItem("CONTEXT/SkinnedMeshRenderer/Paste BlendShapes By Name", false, 120)]
+        static private void PasteBlendShapeByName(MenuCommand cmd) {
+            if (_copiedBlendShapes == null) {
+                Debug.LogAssertion("PasteBlendShapeByName: _copiedBlendShapes is null");
+                return;
+            }
+
+            Undo.RecordObject(cmd.context, "Paste BlendShapes By Name");
+            MovBlendShapes(cmd.context as SkinnedMeshRenderer);
+        }
+
+        // Copy _copiedBlendShapes values to smrCmp
+        static private void MovBlendShapes(SkinnedMeshRenderer smrCmp) {
+            Mesh mesh = smrCmp.sharedMesh;
+
+            foreach(var (s, f) in _copiedBlendShapes) {
+                int idx = mesh.GetBlendShapeIndex(s);
+
+                if (idx != -1) {
+                    smrCmp.SetBlendShapeWeight(idx, f);
+                }
+            }
+        }
+
+        // extarct blendshape's name and value and return as list
+        static private List<(string, float)> ExtractBlendShapes(SkinnedMeshRenderer smr) {
+            List<(string, float)> blendShapes = new List<(string, float)>();
+            Mesh mesh = smr.sharedMesh;
+            int shape_num = mesh.blendShapeCount;
+
+            for (int i = 0; i < shape_num; i++) {
+                blendShapes.Add((mesh.GetBlendShapeName(i), smr.GetBlendShapeWeight(i)));
+            }
+
+            return blendShapes;
+        }
+
+        // naive implementation
+        // compare 2 BlendShape Lists by their name
+        // doesn't matter name order
+        static private bool IsSameBlendShapes(List<(string, float)> x, List<(string, float)> y) {
+            HashSet<string> blendShapeNames_x = new HashSet<string>();
+            HashSet<string> blendShapeNames_y = new HashSet<string>();
+
+            x.ForEach(sf => blendShapeNames_x.Add(sf.Item1));
+            y.ForEach(sf => blendShapeNames_y.Add(sf.Item1));
+
+            return blendShapeNames_x.SetEquals(blendShapeNames_y);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# BlendShapeCopyTool
+This Unity extension assists in copying Blend Shape values to other Skinned Mesh Render's Blend Shape.
+
+# How To Use
+1. Right click on Skinned Mesh Render you want to copy from
+2. Click "Copy BlendShapes"
+3. Select the mesh you want to copy to
+4. Right click on its mesh's Skinned Mesh Render
+5. Click "Paste BlendShapes Strictly" or "Paste BlendShapes By Name"
+
+In "Paste BlendShapes Strictly", check that both meshes has same blend shape's name and do nothing if not same.
+
+In "Paste BlendShapes By Name", copy blend shape values only when each name's is the same.
